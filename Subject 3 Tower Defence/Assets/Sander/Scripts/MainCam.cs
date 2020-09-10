05d80f8768fab4d42640497c91b522496218939f
Sander Y B <sanderybrinkman@live.nl>
2020-09-10 11:07:02 +0200

    added towerplacment and camara zoom

---
+++ Subject 3 Tower Defence/Assets/Sander/Scripts/MainCam.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCam : MonoBehaviour
+{
+    public float camRotX;
+    public float camRotY;
+
+    Camera mainCam;
+    public Vector3 camOffset;
+    public GameObject camFocusObject;
+
+    public float minZoom = 20;
+    public float maxZoom = 60;
+    public float zoomSpeed = 20;
+    float zoomValue = 60;
+
+
+    public GameObject focusGun;
+    public Vector3 focusGunOffset;
+    public bool isGunFocus;
+    public float gunZoom = 35;
+    
+    
+
+    [HideInInspector]
+    public bool isZoomed;
+    
+    
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        mainCam = Camera.main;
+
+        SetDefaultCamPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CamZoom();
+        FocusOnGun();
+    }
+
+
+    void SetDefaultCamPos()
+    {
+        transform.position = camFocusObject.transform.position + camOffset;
+        mainCam.transform.rotation = Quaternion.Euler(camRotX, camRotY, 0f);
+    }
+    
+    void CamZoom()
+    {
+        if (isGunFocus)
+        {
+            if (!isZoomed)
+            {
+                if (Input.GetButtonDown("Fire3"))
+                {
+                    Camera.main.fieldOfView = gunZoom;
+                }
+            }
+            else
+            {
+                if (Input.GetButtonDown("Fire3"))
+                {
+                    Camera.main.fieldOfView = maxZoom;
+                }
+            }
+        }
+        else
+        {
+            zoomValue -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+
+            zoomValue = Mathf.Clamp(zoomValue, minZoom, maxZoom);
+
+            isZoomed = zoomValue < maxZoom;
+
+            Camera.main.fieldOfView = zoomValue;
+        }
+    }
+
+    void FocusOnGun()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+
+            if (!isGunFocus)
+            {
+                
+                transform.position = focusGun.transform.position + focusGunOffset;
+                transform.rotation = focusGun.transform.rotation;
+                isGunFocus = true;
+            }
+            else
+            {
+                SetDefaultCamPos();
+                isGunFocus = false;
+            }
+            Camera.main.fieldOfView = maxZoom;
+        }
+    }
+
+}
---
+++ Subject 3 Tower Defence/Assets/Sander/Scripts/TowerBuilder.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBuilder : MonoBehaviour
+{
+    public GameObject[] towerPrefabs;
+
+    private GameObject currentHeldTower;
+
+     public TowerManager manager;
+
+
+    void Start()
+    {
+        foreach (GameObject tower in towerPrefabs)
+        {
+            //add number key for tower place input
+        }
+    }
+
+    void Update()
+    {
+        // dit block is voor debug only en moet met Ui buttons
+        if (Input.GetButtonDown("Submit")) 
+        {
+           SpawnNewTower(0);
+        }
+        if (currentHeldTower)
+        {
+
+            MoveTowerToMouse();
+            ConfirmPlace();
+        }
+    }
+
+    private void SpawnNewTower(int index)
+    {
+        if (!currentHeldTower)
+        {
+            currentHeldTower = Instantiate(towerPrefabs[index], towerPrefabs[index].transform.position, Quaternion.identity);
+            manager.SelectTower(currentHeldTower.GetComponent<BaseTower>());
+        }
+        else
+        {
+            CancelPlacement();
+        }
+
+    }
+
+    private void MoveTowerToMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            currentHeldTower.transform.position = hitInfo.point;
+        }
+    }
+
+    private void ConfirmPlace()
+    {
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (!manager.cantPlace)
+            {
+                manager.PlacedTower(currentHeldTower.GetComponent<BaseTower>());
+                manager.DeselectTower();
+                currentHeldTower = null;
+
+                //currency - tower cost
+            }
+        }
+    }
+
+    public void CancelPlacement()
+    {
+        manager.DeselectTower();
+        Destroy(currentHeldTower);
+    }
+
+
+
+
+}
